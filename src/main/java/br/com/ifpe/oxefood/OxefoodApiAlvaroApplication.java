0d94c3aedd813cd66fe5aedc720e79ab03e6e136
Álvaro H. S. de Carvalho <91854253+AlvaroHSC@users.noreply.github.com>
2024-02-25 13:49:21 -0300

    configurando todo projeto em casa

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiAlvaroApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiAlvaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiAlvaroApplication.class, args);
+	}
+
+}
