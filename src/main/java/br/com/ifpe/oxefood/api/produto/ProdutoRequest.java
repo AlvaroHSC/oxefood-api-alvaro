69096a545398780792320d4c91a39c186dd0ef77
Álvaro H. S. de Carvalho <91854253+AlvaroHSC@users.noreply.github.com>
2024-03-17 21:40:36 -0300

    Estudando a implementação de entregador já gravando com sucesso no bd

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoRequest.java
@@ -1,11 +1,6 @@
 package br.com.ifpe.oxefood.api.produto;
 
-import java.time.LocalDate;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import br.com.ifpe.oxefood.modelo.produto.Produto;
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
