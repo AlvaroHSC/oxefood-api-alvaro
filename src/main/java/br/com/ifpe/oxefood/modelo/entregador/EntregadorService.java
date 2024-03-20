16d2cf0b0f14c9a98ea51047150e4f3ce7888779
aluno <atendimento.secretaria@jaboatao.ifpe.edu.br>
2024-03-20 20:55:26 -0300

    implementando o slide c14, rota de update/atualizar o cadastro de produto, cliente e entregador

--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -11,24 +11,48 @@ import jakarta.transaction.Transactional;
 @Service
 public class EntregadorService {
     @Autowired
-   private EntregadorRepository repository;
+    private EntregadorRepository repository;
 
-   @Transactional
-   public Entregador save(Entregador entregador) {
+    @Transactional
+    public Entregador save(Entregador entregador) {
 
-       entregador.setHabilitado(Boolean.TRUE);
-       entregador.setVersao(1L);
-       entregador.setDataCriacao(LocalDate.now());
-       return repository.save(entregador);
-   }
+        entregador.setHabilitado(Boolean.TRUE);
+        entregador.setVersao(1L);
+        entregador.setDataCriacao(LocalDate.now());
+        return repository.save(entregador);
+    }
 
-   public List<Entregador> listarTodos() {
+    public List<Entregador> listarTodos() {
         return repository.findAll();
     }
 
     public Entregador obterPorID(Long id) {
 
         return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, Entregador entregadorAlterado) {
+
+        Entregador entregador = repository.findById(id).get();
+        entregador.setNome(entregadorAlterado.getNome());
+        entregador.setCpf(entregadorAlterado.getCpf());
+        entregador.setRg(entregadorAlterado.getRg());
+        entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+        entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+        entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+        entregador.setQtdEntregasRealizadas(entregadorAlterado.getQtdEntregasRealizadas());
+        entregador.setValorFrete(entregadorAlterado.getValorFrete());
+        entregador.setEnderecoRua(entregadorAlterado.getEnderecoRua());
+        entregador.setEnderecoBairro(entregadorAlterado.getEnderecoBairro());
+        entregador.setEnderecoCidade(entregadorAlterado.getEnderecoCidade());
+        entregador.setEnderecoCep(entregadorAlterado.getEnderecoCep());
+        entregador.setEnderecoUf(entregadorAlterado.getEnderecoUf());
+        entregador.setEnderecoComplemento(entregadorAlterado.getEnderecoComplemento());
+        entregador.setAtivo(entregadorAlterado.getAtivo());
+
+        entregador.setVersao(entregador.getVersao() + 1);
+        repository.save(entregador);
     }
 
 }
